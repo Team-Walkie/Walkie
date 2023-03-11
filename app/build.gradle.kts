1abd4a7afd21d4758907f5895af1e47d37e54c8a
soopeach <hsjeon584@gmail.com>
2023-03-11 21:18:50 +0900

    :wrench: Feature 단위 모듈들 기본 navigation 관련 설정
    Co-authored-by: yonghanJu <ju990828@naver.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,9 +33,17 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
+
+    implementation(project(":running"))
+    implementation(project(":community"))
+    implementation(project(":challenge"))
+    implementation(project(":mypage"))
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
@@ -44,4 +52,13 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    val navVersion = "2.5.3"
+
+    // Kotlin
+    implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
+    implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
+
+    // Feature module Support
+    implementation("androidx.navigation:navigation-dynamic-features-fragment:$navVersion")
 }
