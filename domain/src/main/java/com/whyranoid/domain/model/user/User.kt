38fdba42217dad3a6aab379441c9051f087525e0
soopeach <hsjeon584@gmail.com>
2023-05-15 15:43:45 +0900

    :sparkles: User 더미 데이터 구현

--- domain/src/main/java/com/whyranoid/domain/model/user/User.kt
+++ domain/src/main/java/com/whyranoid/domain/model/user/User.kt
@@ -5,4 +5,37 @@ data class User(
     val name: String,
     val nickname: String,
     val imageUrl: String,
-)
+) {
+    companion object {
+
+        private val DUMMY_NAME_LIST = listOf(
+            "김철수",
+            "김영희",
+            "김영수",
+            "김민수",
+            "김민희",
+            "김민지",
+            "김민수",
+            "곽두팔",
+        )
+
+        private val DUMMY_NICKNAME_LIST = listOf(
+            "하늘을 달리는 소년",
+            "무지개를 담은 소녀",
+            "무지개를 담은 소년",
+            "하늘을 달리는 소녀",
+            "밥 먹고 싶은 사람",
+            "배가 고픈 사람",
+            "집 가고 싶은 사람"
+        )
+
+        val DUMMY
+            get() =
+                User(
+                    uid = "124151234",
+                    name = DUMMY_NAME_LIST.shuffled().first(),
+                    nickname = DUMMY_NICKNAME_LIST.shuffled().first(),
+                    imageUrl = "https://picsum.photos/250/250",
+                )
+    }
+}
