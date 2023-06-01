5faabeaf03cb447b40df7f763d7e1c26740a8401
yonghanJu <ju990828@naver.com>
2023-06-01 18:48:20 +0900

    :sparkles: 뱃지 더미 데이터 추가

--- domain/src/main/java/com/whyranoid/domain/model/challenge/Badge.kt
+++ domain/src/main/java/com/whyranoid/domain/model/challenge/Badge.kt
@@ -4,4 +4,30 @@ data class Badge(
     val id: Long,
     val name: String,
     val imageUrl: String,
-)
+) {
+    companion object {
+        val DUMMY = Badge(
+            id = 0L,
+            name = "badgeName",
+            imageUrl = "https://picsum.photos/250/250",
+        )
+
+        val DUMMY_LIST = listOf(
+            Badge(
+                id = 0L,
+                name = "badgeName1",
+                imageUrl = "https://picsum.photos/250/250",
+            ),
+            Badge(
+                id = 1L,
+                name = "badgeName2",
+                imageUrl = "https://picsum.photos/250/250",
+            ),
+            Badge(
+                id = 2L,
+                name = "badgeName3",
+                imageUrl = "https://picsum.photos/250/250",
+            ),
+        )
+    }
+}
