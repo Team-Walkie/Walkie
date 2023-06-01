9a0b59c6e8bd2e3495a4143baafc665913acf58e
yonghanJu <ju990828@naver.com>
2023-06-01 19:05:00 +0900

    :sparkles: Post, PostPreview 더미데이터 추가

--- domain/src/main/java/com/whyranoid/domain/model/post/Post.kt
+++ domain/src/main/java/com/whyranoid/domain/model/post/Post.kt
@@ -8,4 +8,14 @@ data class Post(
     val likeCount: Int,
     val contents: String,
     val author: User,
-)
+) {
+    companion object {
+        val DUMMY = Post(
+            id = 0L,
+            imageUrl = "https://picsum.photos/250/250",
+            likeCount = 3,
+            contents = "오늘도 상쾌한 달리기~",
+            author = User.DUMMY,
+        )
+    }
+}
