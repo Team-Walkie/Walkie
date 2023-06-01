7507c446807b295e5e5f0162fedcc7c980e23b39
yonghanJu <ju990828@naver.com>
2023-06-01 18:51:30 +0900

    :sparkles: 첼린지 레포지토리에 뱃지 반환 함수 추가

--- domain/src/main/java/com/whyranoid/domain/repository/ChallengeRepository.kt
+++ domain/src/main/java/com/whyranoid/domain/repository/ChallengeRepository.kt
@@ -1,16 +1,19 @@
 package com.whyranoid.domain.repository
 
+import com.whyranoid.domain.model.challenge.Badge
 import com.whyranoid.domain.model.challenge.Challenge
 import com.whyranoid.domain.model.challenge.ChallengePreview
 import com.whyranoid.domain.model.challenge.ChallengeType
 
 interface ChallengeRepository {
 
-    suspend fun getNewChallengePreviews() : List<ChallengePreview>
+    suspend fun getNewChallengePreviews(): List<ChallengePreview>
 
-    suspend fun getChallengingPreviews() : List<ChallengePreview>
+    suspend fun getChallengingPreviews(): List<ChallengePreview>
 
-    suspend fun getChallengeDetail(challengeId: Long) : Challenge
+    suspend fun getChallengeDetail(challengeId: Long): Challenge
 
-    suspend fun getChallengePreviewsByType(type: ChallengeType) : List<ChallengePreview>
+    suspend fun getChallengePreviewsByType(type: ChallengeType): List<ChallengePreview>
+
+    suspend fun getUserBadges(uid: String): Result<List<Badge>>
 }
