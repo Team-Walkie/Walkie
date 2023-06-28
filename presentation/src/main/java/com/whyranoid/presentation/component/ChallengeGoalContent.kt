ab1e034342cca768d3d6c3a3ae4f5e078d689799
soopeach <hsjeon584@gmail.com>
2023-06-28 15:16:39 +0900

    :sparkles: 챌린지 도전 내용의 progress bar가 아이콘과 진행도 텍스트를 표기하도록 변경

--- presentation/src/main/java/com/whyranoid/presentation/component/ChallengeGoalContent.kt
+++ presentation/src/main/java/com/whyranoid/presentation/component/ChallengeGoalContent.kt
@@ -1,51 +1,175 @@
 package com.whyranoid.presentation.component
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.progressSemantics
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Divider
+import androidx.compose.material.ProgressIndicatorDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.ExperimentalTextApi
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.drawText
+import androidx.compose.ui.text.rememberTextMeasurer
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.whyranoid.domain.model.challenge.Challenge
+import com.whyranoid.presentation.R
 
+@OptIn(ExperimentalTextApi::class)
 @Composable
 fun ChallengeGoalContent(
     challenge: Challenge
 ) {
-    Row(
+
+    Column(
         modifier = Modifier
             .fillMaxWidth()
-            .height(101.dp)
             .clip(shape = RoundedCornerShape(15.dp))
             .background(Color(0xFFF7F7F7)),
-        horizontalArrangement = Arrangement.SpaceEvenly,
-        verticalAlignment = Alignment.CenterVertically
     ) {
-        ChallengeGoalItem(
-            modifier = Modifier.weight(1f),
-            goal = "기간", limit = "${challenge.period}일"
-        )
 
-        Divider(
-            color = Color(0xFFBABABA),
+        val progressBarColor = Color(0xFFFFB763)
+
+        val progress = requireNotNull(challenge.process) / 100f
+
+        Spacer(modifier = Modifier.height(30.dp))
+
+        val option = BitmapFactory.Options().apply {
+            inPreferredConfig = Bitmap.Config.ARGB_8888
+            inScreenDensity
+        }
+
+        val bitmap = BitmapFactory.decodeResource(
+            LocalContext.current.resources,
+            R.drawable.run_icon,
+            option
+        ).asImageBitmap()
+
+        val textMeasure = rememberTextMeasurer()
+
+        Canvas(
             modifier = Modifier
-                .height(52.dp)
-                .width(1.dp)
-        )
+                .progressSemantics(progress)
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp)
+                .height(50.dp)
+        ) {
 
-        // TODO: challenge type
-        ChallengeGoalItem(
-            modifier = Modifier.weight(1f),
-            goal = "칼로리", limit = "1110kcal"
-        )
+            val width = size.width
+            val height = size.height
 
+            val yOffset = height / 2
+
+            val isLtr = layoutDirection == LayoutDirection.Ltr
+            val barStart = (if (isLtr) 0f else 1f - progress) * width
+            val barEnd = (if (isLtr) progress else 1f - 0f) * width
+
+            // draw Background
+            drawLine(
+                progressBarColor
+                    .copy(alpha = ProgressIndicatorDefaults.IndicatorBackgroundOpacity),
+                Offset(0f, yOffset),
+                Offset(1f * width, yOffset),
+                8.dp.toPx(),
+                cap = StrokeCap.Round,
+            )
+
+            // Progress line
+            drawLine(
+                progressBarColor,
+                Offset(barStart, yOffset),
+                Offset(barEnd, yOffset),
+                8.dp.toPx(),
+                cap = StrokeCap.Round,
+            )
+
+            val bitMapIconSize = IntSize(41.dp.toPx().toInt(), 42.dp.toPx().toInt())
+
+            drawImage(
+                bitmap,
+                dstSize = bitMapIconSize,
+                dstOffset = IntOffset(
+                    (barEnd - bitMapIconSize.width / 2).toInt(),
+                    (yOffset - bitMapIconSize.height / 2).toInt()
+                )
+            )
+
+            val progressText = "${(progress * 100).toInt()}%"
+
+            val progressTextMeasured = textMeasure.measure(
+                progressText,
+                TextStyle(
+                    fontSize = 15.sp,
+                )
+            )
+
+            drawText(
+                textMeasurer = textMeasure, text = progressText,
+                style = TextStyle(
+                    fontSize = 15.sp,
+                ),
+                topLeft = Offset(
+                    barEnd
+                            - progressTextMeasured.size.width / 2,
+                    yOffset
+                            - bitMapIconSize.height / 2
+                            - progressTextMeasured.size.height
+
+                ),
+                softWrap = false,
+                overflow = TextOverflow.Visible,
+            )
+
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(101.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            ChallengeGoalItem(
+                modifier = Modifier.weight(1f),
+                goal = "기간", limit = "${challenge.period}일"
+            )
+
+            Divider(
+                color = Color(0xFFBABABA),
+                modifier = Modifier
+                    .height(52.dp)
+                    .width(1.dp)
+            )
+
+            // TODO: challenge type
+            ChallengeGoalItem(
+                modifier = Modifier.weight(1f),
+                goal = "칼로리", limit = "1110kcal"
+            )
+
+        }
     }
 }
