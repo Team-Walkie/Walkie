b23e605ee17bd48b7726e52ba1f9a5ac68c05bb7
yonghanJu <ju990828@naver.com>
2023-07-07 16:04:38 +0900

    :sparkles: 러닝 화면 관련 모델 생성

---
+++ presentation/src/main/java/com/whyranoid/presentation/model/running/RunningInfo.kt
@@ -0,0 +1,9 @@
+package com.whyranoid.presentation.model.running
+
+data class RunningInfo(
+    val distance: Double = 0.0,
+    val runningTime: Int = 0,
+    val pace: Double = 0.0,
+    val calories: Double = 0.0,
+    val steps: Int = 0,
+)
