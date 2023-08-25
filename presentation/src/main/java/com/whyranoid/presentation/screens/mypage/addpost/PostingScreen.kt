f1254e3141eb0bbc50aabb0b155f49140c77da46
yonghanJu <ju990828@naver.com>
2023-08-25 17:43:58 +0900

    :sparkles: 포스팅 화면 구현

--- presentation/src/main/java/com/whyranoid/presentation/screens/mypage/addpost/PostingScreen.kt
+++ presentation/src/main/java/com/whyranoid/presentation/screens/mypage/addpost/PostingScreen.kt
@@ -3,6 +3,8 @@ package com.whyranoid.presentation.screens.mypage.addpost
 import android.annotation.SuppressLint
 import android.util.Log
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -10,6 +12,7 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -17,9 +20,13 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Icon
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -30,8 +37,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.geometry.LatLngBounds
@@ -46,6 +56,7 @@ import com.naver.maps.map.compose.rememberCameraPositionState
 import com.whyranoid.domain.model.running.RunningHistory
 import com.whyranoid.presentation.R
 import com.whyranoid.presentation.model.running.toRunningHistoryUiModel
+import com.whyranoid.presentation.reusable.GalleryGrid
 import com.whyranoid.presentation.screens.running.DrawBitmap
 import com.whyranoid.presentation.screens.running.DrawPath
 import com.whyranoid.presentation.theme.WalkieColor
@@ -59,28 +70,45 @@ import java.util.*
 @SuppressLint("SimpleDateFormat")
 @Composable
 fun PostingScreen(runningHistory: RunningHistory) {
+    var textVisibleState by remember { mutableStateOf(true) }
+    var photoEditState by remember { mutableStateOf(false) }
+
     Column(
         modifier = Modifier
-            .padding(20.dp)
             .fillMaxWidth()
             .wrapContentHeight(),
+        verticalArrangement = Arrangement.Top,
     ) {
-        Box(Modifier.fillMaxWidth()) {
+        Box(
+            Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp)
+                .padding(top = 20.dp),
+        ) {
             Text(
                 style = WalkieTypography.Title,
                 text = "새 게시물",
-                modifier = Modifier.align(Alignment.Center).padding(bottom = 24.dp),
+                modifier = Modifier
+                    .align(Alignment.Center)
+                    .padding(bottom = 24.dp),
             )
         }
 
-        Row(horizontalArrangement = Arrangement.spacedBy(12.dp)) {
+        Row(
+            modifier = Modifier.padding(horizontal = 20.dp),
+            horizontalArrangement = Arrangement.spacedBy(12.dp),
+        ) {
             SimpleDateFormat("yyyy/MM/dd HH:mm").format(Date(runningHistory.finishedAt)).split(' ')
                 .forEach {
                     Row(
                         horizontalArrangement = Arrangement.Center,
                         verticalAlignment = Alignment.CenterVertically,
-                        modifier = Modifier.padding(bottom = 12.dp).weight(1f).height(40.dp)
-                            .clip(RoundedCornerShape(12.dp)).background(WalkieColor.GrayDefault),
+                        modifier = Modifier
+                            .padding(bottom = 12.dp)
+                            .weight(1f)
+                            .height(40.dp)
+                            .clip(RoundedCornerShape(12.dp))
+                            .background(WalkieColor.GrayDefault),
                     ) {
                         Text(text = it, style = WalkieTypography.SubTitle)
                     }
@@ -89,46 +117,104 @@ fun PostingScreen(runningHistory: RunningHistory) {
 
         var runningHistoryState by remember { mutableStateOf(runningHistory) }
 
-        Map(runningHistoryState) { uri ->
-            runningHistoryState = runningHistoryState.copy(bitmap = uri)
-        }
+        Map(runningHistoryState, textVisibleState)
 
         Row(
             horizontalArrangement = Arrangement.SpaceAround,
-            modifier = Modifier.align(Alignment.End).wrapContentSize(),
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .padding(horizontal = 20.dp)
+                .align(Alignment.End)
+                .wrapContentSize(),
         ) {
+            if (photoEditState) {
+                Text("앨범", style = WalkieTypography.SubTitle)
+                Spacer(Modifier.weight(1f))
+            }
             Icon(
-                modifier = Modifier.size(48.dp).padding(12.dp),
+                modifier = Modifier
+                    .clickable {
+                        textVisibleState = textVisibleState.not()
+                    }
+                    .size(48.dp)
+                    .padding(12.dp),
                 painter = painterResource(id = R.drawable.ic_timer),
-                contentDescription = "gallery",
+                contentDescription = "textVisible",
                 tint = WalkieColor.GrayDefault,
             )
             Icon(
-                modifier = Modifier.size(48.dp).padding(12.dp),
+                modifier = Modifier
+                    .clickable {
+                        photoEditState = photoEditState.not()
+                    }
+                    .size(48.dp)
+                    .padding(12.dp),
                 painter = painterResource(id = R.drawable.ic_gallery),
                 contentDescription = "gallery",
                 tint = WalkieColor.GrayDefault,
             )
         }
 
-        // Text 박스
+        val focusManager = LocalFocusManager.current
+        var text by remember { mutableStateOf("") }
 
-        Spacer(Modifier.weight(1f))
+        if (photoEditState) {
+            GalleryGrid(column = 3) { uri ->
+                runningHistoryState = runningHistoryState.copy(bitmap = uri.toString())
+            }
+        } else {
+            BasicTextField(
+                keyboardActions = KeyboardActions(onDone = { focusManager.clearFocus() }),
+                keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done),
+                modifier = Modifier
+                    .padding(horizontal = 20.dp)
+                    .fillMaxWidth()
+                    .height(92.dp)
+                    .background(Color.White)
+                    .border(1.dp, WalkieColor.GrayDefault, RoundedCornerShape(12.dp))
+                    .padding(8.dp),
+                value = text,
+                onValueChange = {
+                    text = it
+                },
+                singleLine = false,
+                cursorBrush = SolidColor(MaterialTheme.colorScheme.primary),
+            )
+        }
+    }
 
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp),
+        contentAlignment = Alignment.BottomCenter,
+    ) {
         Button(
             shape = RoundedCornerShape(12.dp),
-            modifier = Modifier.fillMaxWidth().height(48.dp),
-            onClick = { /* TODO */ },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(48.dp),
+            onClick = {
+                if (photoEditState) {
+                    photoEditState = false
+                } else {
+                    // TODO 게시글 올리기
+                }
+            },
             colors = ButtonDefaults.buttonColors(containerColor = WalkieColor.Primary),
         ) {
-            Text(text = "올리기", color = Color.White)
+            Text(text = if (photoEditState) "확인" else "올리기", color = Color.White)
         }
     }
 }
 
+@SuppressLint("SimpleDateFormat")
 @OptIn(ExperimentalNaverMapApi::class)
 @Composable
-fun Map(runningHistory: RunningHistory, onImageSelected: (uri: String) -> Unit) {
+fun Map(
+    runningHistory: RunningHistory,
+    textVisibleState: Boolean,
+) {
     val runningHistoryUiModel = runningHistory.toRunningHistoryUiModel(LocalContext.current)
 
     val cameraPositionState = rememberCameraPositionState()
@@ -175,7 +261,9 @@ fun Map(runningHistory: RunningHistory, onImageSelected: (uri: String) -> Unit) 
 
     Box {
         NaverMap(
-            modifier = Modifier.aspectRatio(1f),
+            modifier = Modifier
+                .padding(horizontal = 20.dp)
+                .aspectRatio(1f),
             cameraPositionState = cameraPositionState,
             uiSettings = mapUiSettings,
             properties = mapProperties,
@@ -201,24 +289,33 @@ fun Map(runningHistory: RunningHistory, onImageSelected: (uri: String) -> Unit) 
             }
         }
 
+        Text(
+            text = SimpleDateFormat("yyyy.MM.dd HH:mm").format(Date(runningHistory.finishedAt)),
+            modifier = Modifier.padding(top = 12.dp).align(Alignment.TopCenter),
+            style = WalkieTypography.Body2,
+        )
+
         // 지도 하단 정보
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .align(Alignment.BottomCenter)
-                .padding(bottom = 12.dp)
-                .padding(horizontal = 20.dp),
-            horizontalArrangement = Arrangement.SpaceBetween,
-        ) {
-            listOf(
-                "%.2f".format(runningHistoryUiModel.distance.div(1000.toDouble())),
-                runningHistoryUiModel.totalRunningTime.toRunningTime(),
-                runningHistoryUiModel.pace.toPace(),
-            ).forEach {
-                Text(
-                    it,
-                    style = WalkieTypography.Title,
-                )
+        if (textVisibleState) {
+            Row(
+                modifier = Modifier
+                    .padding(horizontal = 20.dp)
+                    .fillMaxWidth()
+                    .align(Alignment.BottomCenter)
+                    .padding(bottom = 12.dp)
+                    .padding(horizontal = 20.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+            ) {
+                listOf(
+                    "%.2f".format(runningHistoryUiModel.distance.div(1000.toDouble())),
+                    runningHistoryUiModel.totalRunningTime.toRunningTime(),
+                    runningHistoryUiModel.pace.toPace(),
+                ).forEach {
+                    Text(
+                        it,
+                        style = WalkieTypography.Title,
+                    )
+                }
             }
         }
     }
