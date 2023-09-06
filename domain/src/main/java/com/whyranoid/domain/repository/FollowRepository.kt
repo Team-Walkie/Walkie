440f5e1e857c59da9eab1b3a4a8f791cf1a04178
yonghanJu <ju990828@naver.com>
2023-09-06 14:27:24 +0900

    :sparkles: 팔로잉 관련 레파지토리 구현

---
+++ domain/src/main/java/com/whyranoid/domain/repository/FollowRepository.kt
@@ -0,0 +1,15 @@
+package com.whyranoid.domain.repository
+
+import com.whyranoid.domain.model.user.User
+
+interface FollowRepository {
+    suspend fun follow(followerId: Long, followedId: Long): Result<Long>
+
+    suspend fun unfollow(followerId: Long, followedId: Long): Result<Long>
+
+    suspend fun getWalkingFollowings(uid: Long): Result<List<User>>
+
+    suspend fun getFollowings(uid: Long): Result<List<User>>
+
+    suspend fun getFollowers(uid: Long): Result<List<User>>
+}
