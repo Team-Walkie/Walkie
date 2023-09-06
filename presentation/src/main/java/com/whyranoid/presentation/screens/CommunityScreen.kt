aad88addde7c0900a4faa258d516752b749f7f7e
soopeach <hsjeon584@gmail.com>
2023-09-06 23:12:28 +0900

    :sparkles: 커뮤니티 메인 페이지 틀 구현

--- presentation/src/main/java/com/whyranoid/presentation/screens/CommunityScreen.kt
+++ presentation/src/main/java/com/whyranoid/presentation/screens/CommunityScreen.kt
@@ -1,11 +1,34 @@
 package com.whyranoid.presentation.screens
 
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
+import com.whyranoid.presentation.component.community.PostItem
+import com.whyranoid.presentation.component.community.RunningFollowerItem
 
 @Composable
 fun CommunityScreen(
     navController: NavController
 ) {
 
+    LazyColumn {
+
+        item {
+            LazyRow {
+                repeat(10) {
+                    item { RunningFollowerItem() }
+                }
+            }
+        }
+
+        repeat(10) {
+            item {
+                PostItem()
+            }
+        }
+    }
+
 }
+
+
