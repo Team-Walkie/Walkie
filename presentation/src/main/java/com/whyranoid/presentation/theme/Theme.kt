8166d9838ca7b212c792bee60cd1e10d5a5d9e39
soopeach <hsjeon584@gmail.com>
2023-09-07 23:47:00 +0900

    :lipstick: 상단바 및 시스템 네비게이션 바 색상 변경

--- presentation/src/main/java/com/whyranoid/presentation/theme/Theme.kt
+++ presentation/src/main/java/com/whyranoid/presentation/theme/Theme.kt
@@ -7,7 +7,6 @@ import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
@@ -51,8 +50,8 @@ fun WalkieTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = Color.Transparent.toArgb()
-            window.navigationBarColor = Color.Transparent.toArgb()
+            window.statusBarColor = WalkieColor.GrayStatusBar.toArgb()
+            window.navigationBarColor = WalkieColor.GrayStatusBar.toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
     }
