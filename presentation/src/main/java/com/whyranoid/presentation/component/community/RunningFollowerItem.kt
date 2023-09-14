76b2bd8afd26ccc12c61d436867d5d7c0972b475
yonghanJu <ju990828@naver.com>
2023-09-14 17:37:27 +0900

    Revert ":sparkles: RunningFollowerItem 러닝화면 재활용을 위한 매개변수 및 위치 변경"
    This reverts commit 2ba0a849b4573cf8888d05c93ecbc8a0fca1ce63.

--- presentation/src/main/java/com/whyranoid/presentation/component/community/RunningFollowerItem.kt
+++ presentation/src/main/java/com/whyranoid/presentation/component/community/RunningFollowerItem.kt
@@ -1,4 +1,4 @@
-package com.whyranoid.presentation.reusable
+package com.whyranoid.presentation.component.community
 
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
@@ -15,40 +15,34 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
-import com.whyranoid.domain.model.user.User
 import com.whyranoid.presentation.theme.WalkieColor
 
 @Composable
-fun RunningFollowerItem(
-    user: User = User.DUMMY,
-    isDisplayName: Boolean = true,
-) {
+fun RunningFollowerItem() {
     Column(
         modifier = Modifier.padding(10.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
+
         Box(
             modifier = Modifier
                 .size(76.dp)
                 .border(2.dp, WalkieColor.Primary, CircleShape)
                 .clip(CircleShape),
-            contentAlignment = Alignment.Center,
+            contentAlignment = Alignment.Center
         ) {
             AsyncImage(
-                model = "https://picsum.photos/250/250 ",
-                contentDescription = "달리고 있는 친구의 프로필 이미지",
+                model = "https://picsum.photos/250/250 ", contentDescription = "달리고 있는 친구의 프로필 이미지",
                 modifier = Modifier
                     .size(65.dp)
                     .border(0.5.dp, WalkieColor.GrayBorder, CircleShape)
                     .clip(CircleShape),
-                contentScale = ContentScale.Crop,
+                contentScale = ContentScale.Crop
             )
         }
 
-        if (isDisplayName) {
-            Spacer(modifier = Modifier.size(2.dp))
+        Spacer(modifier = Modifier.size(2.dp))
 
-            Text(text = "내 기록")
-        }
+        Text(text = "내 기록")
     }
 }
