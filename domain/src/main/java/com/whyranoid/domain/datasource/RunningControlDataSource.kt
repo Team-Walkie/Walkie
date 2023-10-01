afbf10475ea13e8eefd3b34cd3582cfe0c56ff42
yonghanJu <ju990828@naver.com>
2023-10-01 17:21:10 +0900

    :sparkles: RunningControlDataSource.kt 생성

---
+++ domain/src/main/java/com/whyranoid/domain/datasource/RunningControlDataSource.kt
@@ -0,0 +1,19 @@
+package com.whyranoid.domain.datasource
+
+import com.whyranoid.domain.model.user.User
+
+interface RunningControlDataSource {
+    suspend fun runningStart(id: Long): Result<Long>
+
+    suspend fun sendLike(id: Long, receiverId: Long): Result<Long>
+
+    suspend fun getTotalLiker(
+        id: Long,
+        authId: String,
+    ): Result<List<User>>
+
+    suspend fun getLikeCount(
+        id: Long,
+        authId: String,
+    ): Result<Long>
+}
