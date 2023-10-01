235870946d4b5f86125fb38ea04e887aba4eb3fe
yonghanJu <ju990828@naver.com>
2023-10-01 16:54:18 +0900

    :sparkles: RunningService.kt 레트로핏 서비스 생성

---
+++ data/src/main/java/com/whyranoid/data/datasource/running/RunningService.kt
@@ -0,0 +1,31 @@
+package com.whyranoid.data.datasource.running
+
+import com.whyranoid.data.API
+import com.whyranoid.data.model.running.LikeTotalResponse
+import com.whyranoid.data.model.running.RunningStartRequest
+import com.whyranoid.data.model.running.SendLikeRequest
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Query
+
+interface RunningService {
+    @POST(API.WalkingControl.RUNNING_START)
+    suspend fun runningStart(@Body runningStartRequest: RunningStartRequest): Response<Long>
+
+    @POST(API.WalkingControl.SEND_LIKE)
+    suspend fun sendLike(@Body sendLikeRequest: SendLikeRequest): Response<LikeTotalResponse>
+
+    @GET(API.WalkingControl.LIKE_TOTAL)
+    suspend fun getTotalLiker(
+        @Query("walkieId") id: Long,
+        @Query("authId") authId: String,
+    ): Response<LikeTotalResponse>
+
+    @GET(API.WalkingControl.LIKE_COUNT)
+    suspend fun getLikeCount(
+        @Query("walkieId") id: Long,
+        @Query("authId") authId: String,
+    ): Response<Long>
+}
