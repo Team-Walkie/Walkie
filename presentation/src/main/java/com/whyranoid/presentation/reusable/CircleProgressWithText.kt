20ab21ba77831a3d2a67a1bc469a21b10207a0b0
yonghanJu <ju990828@naver.com>
2023-10-04 17:44:31 +0900

    :sparkles: CircleProgressWithText.kt Preview 추가

--- presentation/src/main/java/com/whyranoid/presentation/reusable/CircleProgressWithText.kt
+++ presentation/src/main/java/com/whyranoid/presentation/reusable/CircleProgressWithText.kt
@@ -17,8 +17,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.whyranoid.presentation.theme.WalkieColor
+import com.whyranoid.presentation.theme.WalkieTheme
 import com.whyranoid.presentation.theme.WalkieTypography
 
 @Composable
@@ -38,5 +40,13 @@ fun CircleProgressWithText(
             Spacer(modifier = Modifier.height(20.dp))
             CircularProgressIndicator()
         }
+    }
+}
+
+@Composable
+@Preview
+fun CircleProgressWithTextPreview() {
+    WalkieTheme {
+        CircleProgressWithText(text = "로딩 중")
     }
 }
