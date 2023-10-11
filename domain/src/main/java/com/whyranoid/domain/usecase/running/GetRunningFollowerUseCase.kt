55e284e702bc815bf3261b7fd5c6ed9f4cab81e8
yonghanJu <ju990828@naver.com>
2023-10-11 17:20:12 +0900

    :sparkles: GetRunningFollowerUseCase.kt 생성

--- domain/src/main/java/com/whyranoid/domain/usecase/running/GetRunningFollowerUseCase.kt
+++ domain/src/main/java/com/whyranoid/domain/usecase/running/GetRunningFollowerUseCase.kt
@@ -1,3 +1,36 @@
 package com.whyranoid.domain.usecase.running
 
-class GetRunningFollowerUseCase
+import com.whyranoid.domain.model.user.User
+import com.whyranoid.domain.repository.AccountRepository
+import com.whyranoid.domain.repository.FollowRepository
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.first
+
+class GetRunningFollowerUseCase(
+    private val accountRepository: AccountRepository,
+    private val followRepository: FollowRepository,
+) {
+    suspend operator fun invoke(): Flow<Pair<List<User>, List<User>>> {
+        var id = -1L
+        var followings = listOf<User>()
+        var runningFollowings = listOf<User>()
+        kotlin.runCatching {
+            id = requireNotNull(accountRepository.uId.first())
+        }
+        return callbackFlow {
+            while (true) {
+                delay(1000)
+                val followingsResponse = followRepository.getFollowings(id).getOrDefault(listOf())
+                val runningFollowingsResponse =
+                    followRepository.getWalkingFollowings(id).getOrDefault(listOf())
+                if (followings != followingsResponse) {
+                    followings = followingsResponse
+                    runningFollowings = runningFollowingsResponse
+                    send(Pair(runningFollowings, followings - runningFollowings.toSet()))
+                }
+            }
+        }
+    }
+}
