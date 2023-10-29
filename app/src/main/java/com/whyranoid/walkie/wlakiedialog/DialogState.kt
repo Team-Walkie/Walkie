eed180163952d17459ea8a6ddbc97c37876667e1
yonghanJu <ju990828@naver.com>
2023-10-29 14:46:00 +0900

    :bug: #49 클래스명 변경

--- app/src/main/java/com/whyranoid/walkie/wlakiedialog/DialogState.kt
+++ app/src/main/java/com/whyranoid/walkie/wlakiedialog/DialogState.kt
@@ -1,9 +1,11 @@
 package com.whyranoid.walkie.wlakiedialog
 
+import com.whyranoid.walkie.walkiedialog.DialogContentProvider
+
 sealed class DialogState {
     object Initialized : DialogState()
 
     object Valid : DialogState()
 
-    data class InValid(val dialogProvider: DialogProvider) : DialogState()
+    data class InValid(val dialogContentProvider: DialogContentProvider) : DialogState()
 }
