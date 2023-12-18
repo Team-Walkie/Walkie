6748896ff6616148b7bb3b8159da31bd671b4a0c
yonghanJu <ju990828@naver.com>
2023-12-18 13:53:37 +0900

    :sparkles: uid 받아오는 함수 변경

--- domain/src/main/java/com/whyranoid/domain/usecase/community/GetSearchedUserUseCase.kt
+++ domain/src/main/java/com/whyranoid/domain/usecase/community/GetSearchedUserUseCase.kt
@@ -3,16 +3,14 @@ package com.whyranoid.domain.usecase.community
 import com.whyranoid.domain.model.user.UserWithFollowingState
 import com.whyranoid.domain.repository.AccountRepository
 import com.whyranoid.domain.repository.FollowRepository
-import kotlinx.coroutines.flow.first
 
 class GetSearchedUserUseCase(
     private val accountRepository: AccountRepository,
     private val followRepository: FollowRepository,
 ) {
-    // operator fun invoke(query: String) = otherUserRepository.searchUsers(query).flow
     suspend operator fun invoke(query: String): Result<List<UserWithFollowingState>> {
         return kotlin.runCatching {
-            val uid = requireNotNull(accountRepository.uId.first())
+            val uid = accountRepository.getUID()
             val followingList = requireNotNull(followRepository.getFollowings(uid).getOrNull())
             requireNotNull(followRepository.searchNickname(query).getOrNull()).map { user ->
                 UserWithFollowingState(
