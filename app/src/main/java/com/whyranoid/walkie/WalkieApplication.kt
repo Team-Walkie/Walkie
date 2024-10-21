ed58055aac9d2126e361cd77aac022e22a6a996a
yonghanJu <ju990828@naver.com>
2024-10-21 22:04:17 +0900

    :sparkles: SocketTimeoutException 기본 예외 핸들러 설정

--- app/src/main/java/com/whyranoid/walkie/WalkieApplication.kt
+++ app/src/main/java/com/whyranoid/walkie/WalkieApplication.kt
@@ -4,6 +4,9 @@ import android.app.Application
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import java.net.SocketTimeoutException
+
+
 
 class WalkieApplication : Application() {
     override fun onCreate() {
@@ -23,5 +26,13 @@ class WalkieApplication : Application() {
         }
 
         super.onCreate()
+
+        // 기본 예외 핸들러 설정
+        Thread.setDefaultUncaughtExceptionHandler { _, e ->
+            if (e is SocketTimeoutException) {
+                System.err.println("SocketTimeoutException 발생: " + e.message)
+                // todo dialog 띄워주기
+            }
+        }
     }
 }
