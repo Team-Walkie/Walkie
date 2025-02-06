fb0fe8cc21fb70f1fefc8def3e7ed93ad34b2b61
yonghanJu <ju990828@naver.com>
2025-02-06 21:58:01 +0900

    :bookmark: 버전 1.0.19 업그레이드

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 26
         targetSdk = 33
         versionCode = 1
-        versionName = "1.0.18"
+        versionName = "1.0.19"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
