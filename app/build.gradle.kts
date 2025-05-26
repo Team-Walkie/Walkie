d1d4f66df45a75eec1c3e20fbca04e14e781bb1f
yonghanJu <ju990828@naver.com>
2025-05-26 22:35:52 +0900

    :bookmark: 1.0.21 버전 업그레이드

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.whyranoid.walkie"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0.20"
+        versionCode = 4
+        versionName = "1.0.21"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
